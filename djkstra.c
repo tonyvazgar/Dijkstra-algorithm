1f4b8963350cbb7c58d93a1528c6c5ce80581014
tonyvazgar <luis.vazquezga@udlap.mx>
2019-02-03 14:07:41 -0600

    code updated and more understandable

--- djkstra.c
+++ djkstra.c
@@ -1,91 +1,104 @@
-#include<stdio.h>
-#include<curses.h> 						//#include<conio.h> para windows
-#define INFINITY 9999
-#define MAX 10
- 
-void dijkstra(int G[MAX][MAX],int n,int startnode);
- 
-int main() {
-	int G[MAX][MAX],i,j,n,u;
-	printf("Enter no. of vertices:");
-	scanf("%d",&n);
-	printf("\nEnter the adjacency matrix:\n");
-	
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
-			scanf("%d",&G[i][j]);
-		}
-	}
-	printf("\nEnter the starting node:");
-	scanf("%d",&u);
-	dijkstra(G,n,u);
-	
-	return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+//Representing infinity
+#define inf 	100000
+//Number of nodes
+#define V_SIZE	6
+
+//Array fill
+void array_fill(int * array, int len, int val) {
+	int i;
+	for (i = 0; i < len; i++) { array[i] = val; }
 }
- 
-void dijkstra(int G[MAX][MAX],int n,int startnode) {
- 
-	int cost[MAX][MAX],distance[MAX],pred[MAX];
-	int visited[MAX],count,mindistance,nextnode,i,j;
-	
-	//pred[] stores the predecessor of each node
-	//count gives the number of nodes seen so far
-	//create the cost matrix
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
-			if(G[i][j]==0){
-				cost[i][j]=INFINITY;
+
+
+/**
+ * Input parameters：
+ *	graph:	weight matrix
+ *	n:		matrix size (number of nodes)
+ *	start:	starting node
+ *	dist:	where do we are going to put the distance of nodes
+ * */
+void dijkstra(int graph[][V_SIZE], int n, int start, int dist[]) {
+
+	int* path=(int*)malloc(sizeof(int)*n);
+	int* shortest=(int*)malloc(sizeof(int)*n);
+	int* mark=(int*)malloc(sizeof(int)*n);
+	int min, v, i, j;
+
+	array_fill(mark,n, 0);
+	array_fill(dist,n, inf);
+
+	for(i=0; i<n; i++) {
+		dist[i] = graph[start][i];
+		if(i != start && dist[i] < inf){
+            path[i]=start;
+        }else{
+            path[i]=-1;
+        }
+	}
+
+	mark[start]=1;
+	while(1) {
+		min = inf;
+        v = -1;
+		//Encontrar la distancia más pequeña
+		for(i = 0; i < n; i++) {
+			if(!mark[i]) {
+				if(dist[i]<min){
+                    min=dist[i];
+                    v=i;
+                }
 			}
-			else{
-				cost[i][j]=G[i][j];
+		}
+		if(v==-1)break; //Ya no hay ruta más corta
+		//Actualizamos la distancia más corta
+		mark[v]=1;
+		for(i = 0; i < n; i++) {
+			if(!mark[i] &&
+					graph[v][i]!=inf&&
+					dist[v]+graph[v][i]<dist[i]) {
+				dist[i]=dist[v]+graph[v][i];
+				path[i]=v;
 			}
 		}
 	}
-	//initialize pred[],distance[] and visited[]
-	for(i=0;i<n;i++) {
-		distance[i]=cost[startnode][i];
-		pred[i]=startnode;
-		visited[i]=0;
-	}
-	
-	distance[startnode]=0;
-	visited[startnode]=1;
-	count=1;
-	
-	while(count<n-1) {
-		mindistance=INFINITY;
-		
-		//nextnode gives the node at minimum distance
-		for(i=0;i<n;i++){
-			if(distance[i]<mindistance&&!visited[i]) {
-				mindistance=distance[i];
-				nextnode=i;
-			}
+	//Printing the output path
+	printf("Starting node | End node | Source distance | Corresponding path |\n");
+	for(i = 0; i < n; i++) {
+		if(i==start) continue;
+		array_fill(shortest,n, 0);
+		printf("      %d       |",start);
+		printf("     %d    |",i);
+		printf("        %d        | ",dist[i]);
+		int k=0;
+		shortest[k]=i;
+		while(path[shortest[k]]!=start) {
+			k++;shortest[k]=path[shortest[k-1]];
 		}
-			
-		//check if a better path exists through nextnode			
-		visited[nextnode]=1;
-		for(i=0;i<n;i++){
-			if(!visited[i]){
-				if(mindistance+cost[nextnode][i]<distance[i]) {
-					distance[i]=mindistance+cost[nextnode][i];
-					pred[i]=nextnode;
-				}
-			}
+		k++;shortest[k]=start;
+		for(j=k;j>0;j--) {
+			printf("%d->",shortest[j]);
 		}
-		count++;
+		printf("%d\n",shortest[0]);
 	}
- 
-	//print the path and distance of each node
-	for(i=0;i<n;i++){
-		if(i!=startnode) {
-			printf("\nDistance of node%d=%d",i,distance[i]);
-			printf("\nPath=%d",i);
-			j=i;
-			do {
-				j=pred[j];
-				printf("<-%d",j);
-			}while(j!=startnode);
-		}
-	}
+	free(path);
+	free(shortest);
+	free(mark);
+	return ;
+}
+
+int main()
+{
+	int dist[V_SIZE];
+	int W[V_SIZE][V_SIZE] = {
+				{  0,   1,   4,  inf,  inf,  inf },
+				{  1,   0,   2,   7,    5,  inf },
+				{  4,   2,   0,  inf,    1,  inf },
+				{ inf,  7,  inf,   0,    3,    2 },
+				{ inf,  5,    1,   3,   0,    6 },
+				{ inf, inf,  inf,   2,   6,    0 } };
+    dijkstra(W, V_SIZE, 0, dist);
+    return 0;
 }
