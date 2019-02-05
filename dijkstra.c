224a27c62badcc496cabf224dd247893ea6d5689
tonyvazgar <luis.vazquezga@udlap.mx>
2019-02-05 13:16:37 -0600

    update dijkstra.c

--- dijkstra.c
+++ dijkstra.c
@@ -116,41 +116,14 @@ int readmatrix(size_t size, int (*a)[size], const char* filename) {
     return 1;
 }
 
-int main()
-{
+int main() {
 	int dist[V_SIZE];
-	/*int W[V_SIZE][V_SIZE] = {
-				{  0,   1,   4,  inf,  inf,  inf },
-				{  1,   0,   2,   7,    5,  inf },
-				{  4,   2,   0,  inf,    1,  inf },
-				{ inf,  7,  inf,   0,    3,    2 },
-				{ inf,  5,    1,   3,   0,    6 },
-				{ inf, inf,  inf,   2,   6,    0 } };
-    */
-    
+	
     int W[V_SIZE][V_SIZE];
     readmatrix(V_SIZE, W, NAME_FILE);
     
-    /*int W[V_SIZE][V_SIZE] = {
-        {  0,  inf,  inf,  inf,  inf,  inf,  inf,  38,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf },
-        { inf,   0,  inf,  inf,   65,  inf,   64,  inf,   6,  inf,  inf,  inf,  inf,  inf,  inf,  inf },
-        { inf,  inf,  0,  inf,  inf,  inf,  inf,  inf,  inf,  inf,   6,   8,  inf,  inf,  inf,  62 },
-        { inf,  inf,  inf, 0,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf },
-        { inf, 65,  inf,  inf, 0,  inf,  inf,  inf,  inf, 6,  inf,  inf,  inf,  inf, 55,  inf },
-        { inf,  inf,  inf,  inf,  inf, 0,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf },
-        { inf, 64,  inf,  inf,  inf,  inf, 0, 53,  inf,  inf, 36,  inf, 45,  inf,  inf,  inf },
-        {  38,  inf,  inf,  inf,  inf,  inf, 53, 0,  inf,  inf,  inf, 91,  inf, 29,  inf,  inf },
-        { inf, 6,  inf,  inf,  inf,  inf,  inf,  inf, 0,  inf,  inf, 95, 55,  inf,  inf,  inf },
-        { inf,  inf,  inf,  inf, 6,  inf,  inf,  inf,  inf, 0,  inf,  inf,  inf,  inf,  inf,  inf },
-        { inf,  inf, 6,  inf,  inf,  inf, 36,  inf,  inf,  inf, 0,  inf,  inf,  inf,  inf,  inf },
-        { inf, 0 8 0 0 0 0 91 95 0 0 0 60 0 0 0},
-        { inf, 0 0 0 0 0 45 0 55 0 0 60 0 0 0 0},
-        { inf, 0 0 0 0 0 0 29 0 0 0 0 0 0 0 0},
-        { inf, 0 0 0 55 0 0 0 0 0 0 0 0 0 0 0},
-        { inf, 0 62 0 0 0 0 0 0 0 0 0 0 0 0 0}};*/
-    
 	//Printing the matrix to see the representation of the graph
-    for(size_t i = 0; i < V_SIZE; ++i) { for(size_t j = 0; j < V_SIZE; ++j){ printf("[%7d]", W[i][j]); } puts(""); }
+    //for(size_t i = 0; i < V_SIZE; ++i) { for(size_t j = 0; j < V_SIZE; ++j){ printf("[%7d]", W[i][j]); } puts(""); }
     
 	//Do dijkstra in the graph W from 0 node
     dijkstra(W, V_SIZE, STARTING_NODE, dist);
