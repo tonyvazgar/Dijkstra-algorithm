1ded0ababf1a253e95290c2337619eef3f3b6683
tonyvazgar <luis.vazquezga@udlap.mx>
2019-02-04 21:52:00 -0600

    probando archvos

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Output of the program:
 
 Example:
 ![alt text](https://github.com/tonyvazgar/Dijkstra-algorithm/blob/master/example%20working.jpeg?raw=true)
+
+
+Getting files for read from: https://github.com/Hoverbear/CSC-225/tree/master/Assignment%205/tests
+
--- djkstra.c
+++ djkstra.c
@@ -4,7 +4,7 @@
 //Representing infinity
 #define inf 	100000
 //Number of nodes
-#define V_SIZE	6
+#define V_SIZE	64
 
 //Array fill
 void array_fill(int * array, int len, int val) {
@@ -89,16 +89,75 @@ void dijkstra(int graph[][V_SIZE], int n, int start, int dist[]) {
 	return ;
 }
 
+
+int readmatrix(size_t size, int (*a)[size], const char* filename) {
+    FILE *pf;
+    pf = fopen (filename, "r");
+    if (pf == NULL){
+        return 0;
+    }
+    
+    for(size_t i = 0; i < size; ++i) {
+        for(size_t j = 0; j < size; ++j){
+            fscanf(pf, "%d", a[i] + j);
+        }
+    }
+    fclose (pf);
+    
+    for(size_t i= 0; i < size; i++){
+        for(size_t j= 0; j < size; j++){
+            if(a[i][j] == 0 && i != j){
+                a[i][j] = inf;
+            }
+        }
+    }
+    return 1;
+}
+
+
 int main()
 {
 	int dist[V_SIZE];
-	int W[V_SIZE][V_SIZE] = {
+	/*int W[V_SIZE][V_SIZE] = {
 				{  0,   1,   4,  inf,  inf,  inf },
 				{  1,   0,   2,   7,    5,  inf },
 				{  4,   2,   0,  inf,    1,  inf },
 				{ inf,  7,  inf,   0,    3,    2 },
 				{ inf,  5,    1,   3,   0,    6 },
 				{ inf, inf,  inf,   2,   6,    0 } };
+    */
+    
+    int W[V_SIZE][V_SIZE];
+    readmatrix(64, W, "matrix3.txt");
+    
+    
+    
+    /*int W[V_SIZE][V_SIZE] = {
+        {  0,  inf,  inf,  inf,  inf,  inf,  inf,  38,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf },
+        { inf,   0,  inf,  inf,   65,  inf,   64,  inf,   6,  inf,  inf,  inf,  inf,  inf,  inf,  inf },
+        { inf,  inf,  0,  inf,  inf,  inf,  inf,  inf,  inf,  inf,   6,   8,  inf,  inf,  inf,  62 },
+        { inf,  inf,  inf, 0,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf },
+        { inf, 65,  inf,  inf, 0,  inf,  inf,  inf,  inf, 6,  inf,  inf,  inf,  inf, 55,  inf },
+        { inf,  inf,  inf,  inf,  inf, 0,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf,  inf },
+        { inf, 64,  inf,  inf,  inf,  inf, 0, 53,  inf,  inf, 36,  inf, 45,  inf,  inf,  inf },
+        {  38,  inf,  inf,  inf,  inf,  inf, 53, 0,  inf,  inf,  inf, 91,  inf, 29,  inf,  inf },
+        { inf, 6,  inf,  inf,  inf,  inf,  inf,  inf, 0,  inf,  inf, 95, 55,  inf,  inf,  inf },
+        { inf,  inf,  inf,  inf, 6,  inf,  inf,  inf,  inf, 0,  inf,  inf,  inf,  inf,  inf,  inf },
+        { inf,  inf, 6,  inf,  inf,  inf, 36,  inf,  inf,  inf, 0,  inf,  inf,  inf,  inf,  inf },
+        { inf, 0 8 0 0 0 0 91 95 0 0 0 60 0 0 0},
+        { inf, 0 0 0 0 0 45 0 55 0 0 60 0 0 0 0},
+        { inf, 0 0 0 0 0 0 29 0 0 0 0 0 0 0 0},
+        { inf, 0 0 0 55 0 0 0 0 0 0 0 0 0 0 0},
+        { inf, 0 62 0 0 0 0 0 0 0 0 0 0 0 0 0}};*/
+    
+    for(size_t i = 0; i < 64; ++i)
+    {
+        for(size_t j = 0; j < 64; ++j){
+            printf("%9d", W[i][j]);
+        }
+        puts("");
+    }
+    
     dijkstra(W, V_SIZE, 0, dist);
     return 0;
 }
